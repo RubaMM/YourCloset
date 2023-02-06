9b7f6891d43d0541f3786aa44668bfa0af1b2f2f
Ruba <roba8330@gmail.com>
2023-02-06 14:00:30 +0300

    Initial Commit

---
+++ YourCloset/YourClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourClosetApp.swift
+//  YourCloset
+//
+//  Created by Ruba Abuhatlah on 15/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct YourClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
