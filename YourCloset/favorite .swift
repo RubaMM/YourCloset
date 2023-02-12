52eae58709eb6ba4b7b7565ed6212a50c4f39793
leena <107318613+Leenaamir1@users.noreply.github.com>
2023-02-12 16:32:28 +0300

    fav commit

---
+++ YourCloset/favorite .swift
@@ -0,0 +1,20 @@
+//
+//  favorite .swift
+//  YourCloset
+//
+//  Created by leena alajlani on 12/02/2023.
+//
+
+import SwiftUI
+
+struct favorite_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct favorite__Previews: PreviewProvider {
+    static var previews: some View {
+        favorite_()
+    }
+}
