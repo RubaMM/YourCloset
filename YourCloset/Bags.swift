39d23354a077f2d8d1e02a1ecb7faf0bec111b7e
Ruba <roba8330@gmail.com>
2023-02-20 11:36:47 +0300

    d

---
+++ YourCloset/Bags.swift
@@ -0,0 +1,87 @@
+//
+//  Bags.swift
+//  YourCloset
+//
+//  Created by Ruba Abuhatlah on 28/07/1444 AH.
+//
+
+import SwiftUI
+
+struct Bags: View {
+    @EnvironmentObject var vm: ViewModel
+    
+    var body: some View {
+        NavigationView {
+            ZStack (alignment: .top){
+                
+                Color("MainColor").ignoresSafeArea()
+                
+   Divider()
+                
+                HStack{
+                    ZStack{
+                        Rectangle()
+                            .fill(.white)
+                            .frame(width: 170,height:190)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                        
+
+                       
+                            HStack {
+                                Button {
+                                    vm.source = .camera
+                                    vm.showPhotoPicker()
+                                } label: {
+                                    //
+                                    Image("Camera")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 70, height:70 )
+                                }
+                                Button {
+                                    vm.source = .library
+                                    vm.showPhotoPicker()
+                                } label: {
+                                    Text("Photos")
+                                        .foregroundColor(.black)
+                                        .font(.custom("Bodoni 72", size: 28))
+                                }
+                            }
+                        Spacer()
+
+                        
+                    }
+                    Spacer()
+                }.padding()
+                
+        .sheet(isPresented: $vm.showPicker) {
+                        ImagePicker(sourceType: vm.source == .library ? .photoLibrary : .camera, selectedImage: $vm.image)
+                            .ignoresSafeArea()
+                        //end
+                        
+                    }.padding(.trailing,180)
+                
+                    .alert("Error", isPresented: $vm.showCameraAlert, presenting: vm.cameraError, actions: { cameraError in
+                        cameraError.button
+                    }, message: { cameraError in
+                        Text(cameraError.message)
+                    })
+                    
+             
+                    }
+            .navigationTitle("Bags")
+          
+      
+            
+            
+        }
+    }
+}
+
+struct Bags_Previews: PreviewProvider {
+    static var previews: some View {
+        Bags()
+            .environmentObject(ViewModel())
+    }
+}
